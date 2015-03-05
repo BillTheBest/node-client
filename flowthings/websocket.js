cd540f045dae89c6cbee97e2f55757b4bbe6b0ee
Greg Meyer <gregory.matthew.meyer@gmail.com>
2015-03-05 16:14:43 -0500

    adds full websocket support

--- flowthings/websocket.js
+++ flowthings/websocket.js
@@ -1,10 +1,258 @@
 'use strict';
 
 var WebSocket = require('ws');
+var forEach = require('lodash.foreach');
+var partial = require('lodash.partial');
+var extend = require('lodash.assign');
+var map = require('lodash.map');
 
-function BaseWs() {
+var flowthingsWs = {
+  baseMsgId: 0,
+  flow: {
+    objectType: 'flow',
+  },
+  drop: {
+    objectType: 'drop',
+  },
+  track: {
+    objectType: 'track',
+  }
+};
 
-}
+var crudable = {
+  // Should the callback and the listener be the same
+  // or should they be different?
+
+  create: function(obj, params, listener, cb) {
+    if (typeof params === 'function') {
+      cb = listener;
+      listener = params;
+      params = {};
+    } else if (!params) {
+      params = {};
+    }
+
+    var msgId;
+    if (params.msgId) {
+      msgId = params.msgId;
+    } else {
+      msgId = flowthingsWs.baseMsgId; flowthingsWs.baseMsgId += 1;
+    }
+
+    var data = {
+      msgId: msgId,
+      object: this.objectType,
+      type: "create",
+      value: obj
+    };
+
+    data = JSON.stringify(data);
+
+    if (flowthingsWs.readyState === flowthingsWs.OPEN) {
+      flowthingsWs.send(data, {}, cb);
+    } else {
+      flowthingsWs.on('open', function open() {
+        flowthingsWs.send(data, {}, cb);
+      });
+    }
+
+    if (listener) {
+      flowthingsWs.on('message', function(data, flags) {
+        var response = JSON.parse(data);
+        if (response.head && response.head.msgId === msgId) {
+          listener(response, msgId, flags);
+        }
+      });
+    }
+
+
+  },
+
+  read: function(id, params, listener, cb) {
+    if (typeof params === 'function') {
+      cb = listener;
+      listener = params;
+      params = {};
+    } else if (!params) {
+      params = {};
+    }
+
+    var msgId;
+    if (params.msgId) {
+      msgId = params.msgId;
+    } else {
+      msgId = flowthingsWs.baseMsgId; flowthingsWs.baseMsgId += 1;
+    }
+
+    var data = {
+      msgId: msgId,
+      object: this.objectType,
+      type: "find",
+      id: id
+    };
+
+    data = JSON.stringify(data);
+
+    if (flowthingsWs.readyState === flowthingsWs.OPEN) {
+      flowthingsWs.send(data, {}, cb);
+    } else {
+      flowthingsWs.on('open', function open() {
+        flowthingsWs.send(data, {}, cb);
+      });
+    }
+
+    if (listener) {
+      flowthingsWs.on('message', function(data, flags) {
+        var response = JSON.parse(data);
+        if (response.head && response.head.msgId === msgId) {
+          listener(response, msgId, flags);
+        }
+      });
+    }
+  },
+
+  update: function(id, obj, params, listener, cb) {
+    if (typeof params === 'function') {
+      cb = listener;
+      listener = params;
+      params = {};
+    } else if (!params) {
+      params = {};
+    }
+
+    var msgId;
+    if (params.msgId) {
+      msgId = params.msgId;
+    } else {
+      msgId = flowthingsWs.baseMsgId; flowthingsWs.baseMsgId += 1;
+    }
+
+    var data = {
+      msgId: msgId,
+      object: this.objectType,
+      type: "update",
+      id: id,
+      value: obj
+    };
+
+    data = JSON.stringify(data);
+
+    if (flowthingsWs.readyState === flowthingsWs.OPEN) {
+      flowthingsWs.send(data, {}, cb);
+    } else {
+      flowthingsWs.on('open', function open() {
+        flowthingsWs.send(data, {}, cb);
+      });
+    }
+
+    if (listener) {
+      flowthingsWs.on('message', function(data, flags) {
+        var response = JSON.parse(data);
+        if (response.head && response.head.msgId === msgId) {
+          listener(response, msgId, flags);
+        }
+      });
+    }
+  },
+
+  delete: function(id, params, listener, cb) {
+    if (typeof params === 'function') {
+      cb = listener;
+      listener = params;
+      params = {};
+    } else if (!params) {
+      params = {};
+    }
+
+    var msgId;
+    if (params.msgId) {
+      msgId = params.msgId;
+    } else {
+      msgId = flowthingsWs.baseMsgId; flowthingsWs.baseMsgId += 1;
+    }
+
+    var data = {
+      msgId: msgId,
+      object: this.objectType,
+      type: "delete",
+      id: id
+    };
+
+    data = JSON.stringify(data);
+
+    if (flowthingsWs.readyState === flowthingsWs.OPEN) {
+      flowthingsWs.send(data, {}, cb);
+    } else {
+      flowthingsWs.on('open', function open() {
+        flowthingsWs.send(data, {}, cb);
+      });
+    }
+
+    if (listener) {
+      flowthingsWs.on('message', function(data, flags) {
+        var response = JSON.parse(data);
+        if (response.head && response.head.msgId === msgId) {
+          listener(response, msgId, flags);
+        }
+      });
+    }
+  }
+};
+
+var subscribable = {
+  subscribe: function(id, params, messageHandler, cb, listener) {
+    if (typeof params === 'function') {
+      listener = cb;
+      cb = messageHandler;
+      messageHandler = params;
+      params = {};
+    } else if (!params) {
+      params = {};
+    }
+
+    var msgId;
+    if (params.msgId) {
+      msgId = params.msgId;
+    } else {
+      msgId = flowthingsWs.baseMsgId; flowthingsWs.baseMsgId += 1;
+    }
+
+    var data = {
+      msgId: msgId,
+      object: this.objectType,
+      type: "subscribe",
+      flowId: id
+    };
+
+    data = JSON.stringify(data);
+
+    if (flowthingsWs.readyState === flowthingsWs.OPEN) {
+      flowthingsWs.send(data, {}, cb);
+    } else {
+      flowthingsWs.on('open', function open() {
+        flowthingsWs.send(data, {}, cb);
+      });
+    }
+
+    if (listener || messageHandler) {
+      flowthingsWs.on('message', function(data, flags) {
+        var response = JSON.parse(data);
+        if (response.type === "message" && response.resource === id) {
+          if (messageHandler) {
+            messageHandler(response, flags);
+          }
+        }
+
+        if (response.head && response.head.msgId === msgId) {
+          if (listener) {
+            listener(response, msgId, flags);
+          }
+        }
+      });
+    }
+  }
+};
+
 
 exports.wsCb = function(err, data, cb) {
   if (err) return console.log(err);
@@ -21,11 +269,14 @@ exports.wsCb = function(err, data, cb) {
 
   url += '/session/' + sessionId + '/ws';
 
-  var ws = new WebSocket(url);
+  flowthingsWs = extend(new WebSocket(url), flowthingsWs);
+  flowthingsWs.flow = extend(flowthingsWs.flow, subscribable, crudable);
+  flowthingsWs.drop = extend(flowthingsWs.drop, subscribable, crudable);
+  flowthingsWs.track = extend(flowthingsWs.track, subscribable, crudable);
 
   if (cb) {
-    cb(ws);
-  };
+    cb(flowthingsWs);
+  }
 };
 
 
